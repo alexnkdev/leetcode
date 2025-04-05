f9662e77b64fe7cfbe1957306d21bddec4a5f341
alexnkdev <alexnk@amazon.com>
2025-04-05 00:00:54 -0400

    Fix 12. Integer To Roman

--- src/medium/12_IntegerToRoman.java
+++ src/medium/12_IntegerToRoman.java
@@ -1,22 +1,21 @@
 class Solution {
   public String intToRoman(int num) {
-    String digits = Integer.toString(num);
-    Map<String, Integer> symbols = new HashMap<>();
-    symbols.put("I", 1);
-    symbols.put("V", 5);
-    symbols.put("X", 10);
-    symbols.put("L", 50);
-    symbols.put("C", 100);
-    symbols.put("D", 500);
-    symbols.put("M", 1000);
-    symbols.put("XL", 40);
-    symbols.put("IX", 9);
-    symbols.put("IV", 4);
-    symbols.put("XC", 90);
-    symbols.put("CD", 400);
-    symbols.put("CM", 900);
+    Map<String, Integer> symbols = Map.ofEntries(
+        Map.entry("I", 1),
+        Map.entry("V", 5),
+        Map.entry("X", 10),
+        Map.entry("L", 50),
+        Map.entry("C", 100),
+        Map.entry("D", 500),
+        Map.entry("M", 1000),
+        Map.entry("XL", 40),
+        Map.entry("IX", 9),
+        Map.entry("IV", 4),
+        Map.entry("XC", 90),
+        Map.entry("CD", 400),
+        Map.entry("CM", 900));
     int currentValue = num;
-    String ret = "";
+    StringBuilder romanNumeral = new StringBuilder();
     while (currentValue != 0) {
       String maxSubtract = null;
       for (String symbol : symbols.keySet()) {
@@ -26,8 +25,8 @@ class Solution {
         }
       }
       currentValue -= symbols.get(maxSubtract);
-      ret += maxSubtract;
+      romanNumeral.append(maxSubtract);
     }
-    return ret;
+    return romanNumeral.toString();
   }
 }
