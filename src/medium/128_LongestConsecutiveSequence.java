d00641e652bd706733191fbb01e579a2de55ca79
alexnkdev <alexnk@amazon.com>
2025-04-20 02:20:06 -0400

    128. Longest Consecutive Sequence

--- src/medium/128_LongestConsecutiveSequence.java
+++ src/medium/128_LongestConsecutiveSequence.java
@@ -5,24 +5,16 @@ class Solution {
       vals.add(nums[i]);
     }
     int ret = 0;
-    Set<Integer> used = new HashSet<>();
     for (Integer current : vals) {
-      if (used.contains(current)) {
+      if (vals.contains(current - 1)) {
         continue;
       }
-      used.add(current);
       int totalChainLength = 0;
-      int curLeft = current;
-      while (vals.contains(curLeft - 1)) {
-        curLeft--;
-        used.add(curLeft);
-      }
       int curRight = current;
       while (vals.contains(curRight + 1)) {
         curRight++;
-        used.add(curRight);
       }
-      totalChainLength = curRight - curLeft + 1;
+      totalChainLength = curRight - current + 1;
       ret = Math.max(ret, totalChainLength);
     }
     return ret;
